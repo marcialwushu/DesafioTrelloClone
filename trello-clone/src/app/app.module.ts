c92ebf7f753f3caededf1794d9346b030860dd6a
Cleilson <marcialwushu@gmail.com>
2020-08-11 19:21:19 -0300

    NavbarComponent create and MaterialDesignModule coponetization

--- trello-clone/src/app/app.module.ts
+++ trello-clone/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 
 import { NgModule } from '@angular/core';
-import { MatCardModule, MatIconModule } from '@angular/material';
-import { DragDropModule  } from '@angular/cdk/drag-drop';
+// import { MatCardModule, MatIconModule, MatToolbarModule } from '@angular/material';
+// import { DragDropModule  } from '@angular/cdk/drag-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
 import { BoardListComponent } from './board-list/board-list.component';
 import { NavbarComponent } from './navbar/navbar.component';
+
+import { MaterialDesignModule } from './material-design/material-design.module';
 
 @NgModule({
   declarations: [
@@ -21,9 +23,8 @@ import { NavbarComponent } from './navbar/navbar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatCardModule,
-    DragDropModule,
-    MatIconModule
+    MaterialDesignModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ trello-clone/src/app/material-design/material-design.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { MatCardModule, MatIconModule, MatToolbarModule } from '@angular/material';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatCardModule, MatIconModule, MatToolbarModule
+  ],
+  exports: [
+    MatCardModule, MatIconModule, MatToolbarModule
+  ]
+})
+export class MaterialDesignModule { }
