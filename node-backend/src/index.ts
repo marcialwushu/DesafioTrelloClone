cece515ddc666a16055a4b44da2308bf147f98dc
Cleilson <marcialwushu@gmail.com>
2020-08-11 15:52:52 -0300

    Testing EventEmitter

--- node-backend/src/index.ts
+++ node-backend/src/index.ts
@@ -1,1 +1,17 @@
-console.log('testing');
+import * as EventEmitter from 'events'
+
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+
+myEmitter.on("test", () => {
+    console.log('this happens again')
+})
+
+myEmitter.once("test", () => {
+    console.log('this happens only once')
+})
+
+
+myEmitter.emit('test');
+
