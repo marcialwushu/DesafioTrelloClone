1d61e1bc47d661ec3e97210dc3d64624e7886560
Cleilson <marcialwushu@gmail.com>
2020-08-12 01:32:08 -0300

    node aplication web depencies add

---
+++ node-backend/src/app.ts
@@ -0,0 +1,25 @@
+import * as express from 'express';
+import * as cookieParser from 'cookie-parser';
+import * as logger from 'morgan';
+import * as bodyParser from 'body-parser';
+
+import * as path from 'path';
+
+const app = express();
+
+if(app.get('env') === 'development') {
+    app.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+    });
+}
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
